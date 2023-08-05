e977958264137458aaae3df20f4c8187f132853e
IDAT <IDAT@DESKTOP-K1R8D5N>
2023-08-05 17:32:34 -0500

    Agregar archivos de proyecto.

---
+++ Ecommerce/Models/Producto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Ecommerce.Models
+{
+    public partial class Producto
+    {
+        public Producto()
+        {
+            DetallePedidos = new HashSet<DetallePedido>();
+        }
+
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public bool Igv { get; set; }
+        public string Imagen { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Descuento { get; set; }
+        public string Imagen1 { get; set; }
+        public int IdCategoria { get; set; }
+
+        public virtual Categorium IdCategoriaNavigation { get; set; }
+        public virtual ICollection<DetallePedido> DetallePedidos { get; set; }
+    }
+}
